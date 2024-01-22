d83bfe24a08113338e8ac06938dd3d85b66039f8
Saikat Samadder <126934062+Saikat-Samadder@users.noreply.github.com>
2024-01-22 21:42:07 +0530

    Add files via upload

--- s.js
+++ s.js
@@ -950,17 +950,22 @@
 
 // console.log(res);
 
-let n = 5;
 
-let arr=[0,1,1,2,3,5,8]
 
-arr[0] = 0;
-arr[1] = 1;
+// let n = 5;
 
-for(let i=2;i<n-1;i++){
+// let arr=[0,1,1,2,3,5,8]
+
+// arr[0] = 0;
+// arr[1] = 1;
+
+// for(let i=2;i<n-1;i++){
  
-    arr[i]= arr[i-1]+arr[i-2]
+//     arr[i]= arr[i-1]+arr[i-2]
 
     
-}
-console.log(arr[n-1]);
+// }
+// console.log(arr[n-1]);
+
+
+
